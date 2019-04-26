20cb8ea8d4a05b30b902e82cf97dad9fd34ea1f4
Jake Wright <jake@jakewright.net>
2019-04-26 10:43:30 +0100

    Set extra headers

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -15,6 +15,8 @@ type Proxy struct {
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     if p.api.AllowCrossOrigin {
         w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH")
+        w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     }
 
     p.reverseProxy.ServeHTTP(w, r)
